aac76d1f4b3ff5b28ef4c0509dd3b71e002dc281
ollin-sys <ollin001@gmail.com>
2026-05-25 16:41:16 -0700

    data.js

---
+++ data.js
@@ -0,0 +1,20 @@
+// data.js
+window.MASTER_CATALOG_DATA = [
+    { 
+        name: "Godzilla 2001", 
+        mfg: "Bandai", 
+        year: "2001", 
+        line: "Kaiju Series", 
+        rarity: "common", 
+        icon: "🦖" 
+    },
+    { 
+        name: "Mecha-King Ghidorah", 
+        mfg: "S.H. MonsterArts", 
+        year: "2015", 
+        line: "MonsterArts", 
+        rarity: "grail", 
+        icon: "🤖" 
+    }
+    // Add all your figures here...
+];
